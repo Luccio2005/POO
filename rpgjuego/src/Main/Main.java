82e1d1e90a1061473ebfe83e860faa2cfea3401d
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-08-30 17:18:22 -0500

    a;adiendo nuevo package

---
+++ rpgjuego/src/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("JUEGO RPG");
+
+        Panel_de_Juego paneldejuego = new Panel_de_Juego();
+        System.out.println("Tamaño de pantalla: " + paneldejuego.getAnchoPantalla() + "x" + paneldejuego.getAltoPantalla());
+
+
+        window.add(paneldejuego);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        paneldejuego.startGameThread();
+
+
+    }
+}
---
+++ rpgjuego/src/Main/Teclado.java
@@ -0,0 +1,47 @@
+package Main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Teclado implements KeyListener {
+
+    public boolean arribap, abajop, izquierdap, derechap;
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int codigo = e.getKeyCode();
+        if(codigo == KeyEvent.VK_W){
+            arribap = true;
+        }
+        if(codigo == KeyEvent.VK_S){
+            abajop = true;
+        }
+        if(codigo == KeyEvent.VK_A){
+            izquierdap = true;
+        }
+        if(codigo == KeyEvent.VK_D){
+            derechap = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int codigo = e.getKeyCode();
+        if(codigo == KeyEvent.VK_W){
+            arribap = false;
+        }
+        if(codigo == KeyEvent.VK_S){
+            abajop = false;
+        }
+        if(codigo == KeyEvent.VK_A){
+            izquierdap = false;
+        }
+        if(codigo == KeyEvent.VK_D){
+            derechap = false;
+        }
+
+    }
+}
