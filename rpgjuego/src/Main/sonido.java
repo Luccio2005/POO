20ce3b184dd173082513b6d6a9017c1cc8542f12
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-09-13 16:34:42 -0500

    clase sonidö

---
+++ rpgjuego/src/Main/sonido.java
@@ -0,0 +1,37 @@
+package Main;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class sonido {
+    Clip clip;
+    URL Urlsonido[] = new URL[30];
+
+    public sonido(){
+        Urlsonido[0]= getClass().getResource("/sonido/BlueBoyAdventure.wav");
+        Urlsonido[1]= getClass().getResource("/sonido/coin.wav");
+        Urlsonido[2]= getClass().getResource("/sonido/powerup.wav");
+        Urlsonido[3]= getClass().getResource("/sonido/unlock.wav");
+        Urlsonido[4]= getClass().getResource("/sonido/fanfare.wav");
+    }
+    public void setFile(int i){
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(Urlsonido[i]);
+            clip= AudioSystem.getClip();
+            clip.open(ais);
+
+        } catch (Exception e) {
+        }
+    }
+    public void play(){
+        clip.start();
+    }
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stop(){
+        clip.stop();
+    }
+}
