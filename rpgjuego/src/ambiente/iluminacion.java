8104536d46cdc5ffc719c2112f0d97bdc4d94d37
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-11-24 13:42:40 -0500

    ajustes de opacidad

--- rpgjuego/src/ambiente/iluminacion.java
+++ rpgjuego/src/ambiente/iluminacion.java
@@ -31,20 +31,35 @@ public class iluminacion {
         // subtract the light circle from the screen rectangle
         pantallaarea.subtract(areadelaluz);
         // crear una degradaccion de colores
-        Color color[] = new Color[5];
-        float fraccion[] = new float[5];
+        Color color[] = new Color[12];
+        float fraccion[] = new float[12];
 
-        color[0] = new Color(0,0,0,0f);
-        color[1] = new Color(0,0,0,0.25f);
-        color[2] = new Color(0,0,0,0.5f);
-        color[3] = new Color(0,0,0,0.75f);
-        color[4] = new Color(0,0,0,0.98f);
+        color[0] = new Color(0,0,0,0.1f);
+        color[1] = new Color(0,0,0,0.42f);
+        color[2] = new Color(0,0,0,0.52f);
+        color[3] = new Color(0,0,0,0.61f);
+        color[4] = new Color(0,0,0,0.69f);
+        color[5] = new Color(0,0,0,0.76f);
+        color[6] = new Color(0,0,0,0.82f);
+        color[7] = new Color(0,0,0,0.87f);
+        color[8] = new Color(0,0,0,0.91f);
+        color[9] = new Color(0,0,0,0.94f);
+        color[10] = new Color(0,0,0,0.96f);
+        color[11] = new Color(0,0,0,0.98f);
 
         fraccion[0] = 0f;
-        fraccion[1] = 0.25f;
+        fraccion[1] = 0.4f;
         fraccion[2] = 0.5f;
-        fraccion[3] = 0.75f;
-        fraccion[4] = 1f;
+        fraccion[3] = 0.6f;
+        fraccion[4] = 0.65f;
+        fraccion[5] = 0.7f;
+        fraccion[6] = 0.75f;
+        fraccion[7] = 0.8f;
+        fraccion[8] = 0.85f;
+        fraccion[9] = 0.9f;
+        fraccion[10] = 0.95f;
+        fraccion[11] = 1f;
+
 
         RadialGradientPaint gpaint = new RadialGradientPaint(centrox, centroy, (tamanocirculo/2), fraccion, color);
         g2.setPaint(gpaint);
