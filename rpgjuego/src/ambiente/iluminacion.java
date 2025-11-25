4b41effb74e04a5cee5c08ce23206a0b01436ac6
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-11-25 10:56:40 -0500

    bug fixed iluminacion

--- rpgjuego/src/ambiente/iluminacion.java
+++ rpgjuego/src/ambiente/iluminacion.java
@@ -16,20 +16,9 @@ public class iluminacion {
         // create a buffered image
         filtrodeoscuridad = new BufferedImage(gp.anchoPantalla, gp.altoPantalla, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = (Graphics2D) filtrodeoscuridad.getGraphics();
-        // create a screenseized rectangle area
-        Area pantallaarea = new Area(new Rectangle2D.Double(0,0,gp.anchoPantalla, gp.altoPantalla));
         // consigue el centro x y y del ciculode luz
         int centrox = gp.jugador.pantallax + (gp.tileSize)/2;
         int centroy = gp.jugador.pantallay + (gp.tileSize)/2;
-        // consigue el top izq x y  y del ciculo
-        double x = centrox - (tamanocirculo/2);
-        double y = centroy - (tamanocirculo/2);
-        // crear la forma de ciruclo para ilumnicacion
-        Shape formacirculo = new Ellipse2D.Double(x, y, tamanocirculo, tamanocirculo);
-        // crear el area del circulo
-        Area areadelaluz = new Area(formacirculo);
-        // subtract the light circle from the screen rectangle
-        pantallaarea.subtract(areadelaluz);
         // crear una degradaccion de colores
         Color color[] = new Color[12];
         float fraccion[] = new float[12];
@@ -60,18 +49,13 @@ public class iluminacion {
         fraccion[10] = 0.95f;
         fraccion[11] = 1f;
 
-
         RadialGradientPaint gpaint = new RadialGradientPaint(centrox, centroy, (tamanocirculo/2), fraccion, color);
         g2.setPaint(gpaint);
-        g2.fill(areadelaluz);
-
-        // set a color to draw to rectangle
-        //g2.setColor(new Color(0,0,0,0.95f));
-        // draw the screen rectangle whitout the light circle re
-        g2.fill(pantallaarea);
+        g2.fillRect(0, 0, gp.anchoPantalla, gp.altoPantalla);
         g2.dispose();
     }
     public void dibujar(Graphics2D g2){
+
         g2.drawImage(filtrodeoscuridad, 0, 0, null);
     }
 }
