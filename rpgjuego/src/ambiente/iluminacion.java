0175ec495bbaae54c07c63b2fbc2a1a7137b5898
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-11-26 11:41:27 -0500

    rotacion del dia

--- rpgjuego/src/ambiente/iluminacion.java
+++ rpgjuego/src/ambiente/iluminacion.java
@@ -11,6 +11,14 @@ import java.awt.image.BufferedImage;
 public class iluminacion {
     Panel_de_Juego gp;
     BufferedImage filtrodeoscuridad;
+    int contadordia;
+    float filtroalfa = 0f;
+
+    final int dia = 0;
+    final int anochecer = 1;
+    final int noche = 2;
+    final int amanecer = 3;
+    int estadodia = dia;
 
     public iluminacion(Panel_de_Juego gp){
         this.gp = gp;
@@ -22,7 +30,7 @@ public class iluminacion {
         Graphics2D g2 = (Graphics2D) filtrodeoscuridad.getGraphics();
 
         if(gp.jugador.actualluz == null){
-            g2.setColor(new Color(0,0,0,0.98f));
+            g2.setColor(new Color(0,0,0.1f,0.98f));
         }else{
             // consigue el centro x y y del ciculode luz
             int centrox = gp.jugador.pantallax + (gp.tileSize)/2;
@@ -31,18 +39,18 @@ public class iluminacion {
             Color color[] = new Color[12];
             float fraccion[] = new float[12];
 
-            color[0] = new Color(0,0,0,0.1f);
-            color[1] = new Color(0,0,0,0.42f);
-            color[2] = new Color(0,0,0,0.52f);
-            color[3] = new Color(0,0,0,0.61f);
-            color[4] = new Color(0,0,0,0.69f);
-            color[5] = new Color(0,0,0,0.76f);
-            color[6] = new Color(0,0,0,0.82f);
-            color[7] = new Color(0,0,0,0.87f);
-            color[8] = new Color(0,0,0,0.91f);
-            color[9] = new Color(0,0,0,0.94f);
-            color[10] = new Color(0,0,0,0.96f);
-            color[11] = new Color(0,0,0,0.98f);
+            color[0] = new Color(0,0,0.1f,0.1f);
+            color[1] = new Color(0,0,0.1f,0.42f);
+            color[2] = new Color(0,0,0.1f,0.52f);
+            color[3] = new Color(0,0,0.1f,0.61f);
+            color[4] = new Color(0,0,0.1f,0.69f);
+            color[5] = new Color(0,0,0.1f,0.76f);
+            color[6] = new Color(0,0,0.1f,0.82f);
+            color[7] = new Color(0,0,0.1f,0.87f);
+            color[8] = new Color(0,0,0.1f,0.91f);
+            color[9] = new Color(0,0,0.1f,0.94f);
+            color[10] = new Color(0,0,0.1f,0.96f);
+            color[11] = new Color(0,0,0.1f,0.98f);
 
             fraccion[0] = 0f;
             fraccion[1] = 0.4f;
@@ -68,8 +76,50 @@ public class iluminacion {
             setlightsource();
             gp.jugador.luzactualizada = false;
         }
+        // comprobar el estado del dia
+        if(estadodia == dia){
+            contadordia++;
+            if(contadordia > 600){
+                estadodia = anochecer;
+                contadordia = 0;
+            }
+        }
+        if(estadodia == anochecer){
+            filtroalfa += 0.001f;
+            if(filtroalfa > 1f){
+                filtroalfa = 1f;
+                estadodia = noche;
+            }
+        }
+        if(estadodia == noche){
+            contadordia++;
+            if(contadordia > 600){
+                estadodia = amanecer;
+                contadordia = 0;
+            }
+        }
+        if(estadodia == amanecer){
+            filtroalfa -= 0.001f;
+            if(filtroalfa < 0f){
+                filtroalfa = 0;
+                estadodia = dia;
+            }
+        }
     }
     public void dibujar(Graphics2D g2){
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, filtroalfa));
         g2.drawImage(filtrodeoscuridad, 0, 0, null);
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        //debug
+        String situacion = "";
+        switch (estadodia){
+            case dia: situacion = "Dia"; break;
+            case anochecer: situacion = "Dusk"; break;
+            case noche: situacion = "Noche"; break;
+            case amanecer: situacion = "Dawn"; break;
+        }
+        g2.setColor(Color.white);
+        g2.setFont(g2.getFont().deriveFont(50f));
+        g2.drawString(situacion, 800, 500);
     }
 }
