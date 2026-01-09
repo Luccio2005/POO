f2a8ee5d563cf39a7337e1fb76f32a4ebd8f3974
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2026-01-09 12:29:03 -0500

    interaccion objetos

--- deep/src/Main/entity/Player.java
+++ deep/src/Main/entity/Player.java
@@ -14,6 +14,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
@@ -24,6 +25,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
         setDefaultValues();
@@ -64,6 +67,9 @@ public class Player extends Entity{
             // check tile collision
             collisionOn = false;
             gp.cChecker.checkTile(this);
+            //check object colision
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
             // if collisiom is alse
             if(collisionOn == false){
                 switch (direction){
@@ -89,6 +95,24 @@ public class Player extends Entity{
                     spriteNum = 1;
                 }
                 spriteCounter = 0;
+            }
+        }
+    }
+    public void pickUpObject(int i){
+        if(i != 999){
+            String objectName = gp.obj[i].name;
+            switch (objectName){
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key:"+hasKey);
+                    break;
+                case "Door":
+                    if(hasKey > 0){
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key:"+hasKey);
             }
         }
     }
